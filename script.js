ec91b4db3e078550073a6990251d0ae4ec63aa16
unknownjoao <joaolucasfarias0203@gmai.com>
2023-08-31 02:58:25 -0400

    js open/close menu

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const mobilePage = document.getElementById('media-mobile');
 
 const menuMobileBtnClose = document.getElementById('menu-mobile-button-close')
 
-menuMobileBtn = addEventListener('click', () => {
+menuMobileBtn.addEventListener('click', () => {
     mobilePage.style.display = 'none';
     menuMobileOverlay.style.display = 'block';
 });
 
-menuMobileBtnClose = addEventListener('click', () => {
+menuMobileBtnClose.addEventListener('click', () => {
    mobilePage.style.display = 'block';
    menuMobileOverlay.style.display = 'none'; 
 });
